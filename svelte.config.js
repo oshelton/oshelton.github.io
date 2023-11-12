e8dabfb22ae0cb9770618ceab754c63d77fa8ba4
oshelton <jowenshelton@gmail.com>
2023-11-12 08:42:51 -0500

    #3 - Add Flowbrite Svelt as a dependency and set it up.

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,20 @@
-import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
 			pages: 'docs'
-		}),
+		})
 		/* Disabled until confirmed it is actually necessary ffor my primary profile page.
 		paths: {
 			base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
 		},
 		*/
-	}
+	},
+
+	preprocess: [vitePreprocess({})]
 };
 
 export default config;
