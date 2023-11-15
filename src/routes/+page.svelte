c2aa63a2a652919c873d7729e0667b260ae5ca3d
oshelton <jowenshelton@gmail.com>
2023-11-14 21:18:30 -0500

    #7 - Update home page content.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,19 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<h1>
-	to your new<br />SvelteKit app
-</h1>
+<Heading tag="h1" class="mb-8">Welcome!</Heading>
 
-<h2>
-	try editing <strong>src/routes/+page.svelte</strong>
-</h2>
+<P class="mb-4">
+	This is the profile/portfolio site of Jack Owen Shelton (there's a little picture of me in the top left).
+	If the name and face doesn't ring a bell you are probably in the wrong place.
+</P>
+
+<P class="mb-4">
+	This site is dedicated to various parts of my life and projects I have worked on over the years and giving visitors a better picture of who I am.
+</P>
+
+<P class="mb-8">
+	At some point it may even host a blog.
+</P>
+
+<Heading tag="h5">Thank you for visiting and enjoy your stay!</Heading>
