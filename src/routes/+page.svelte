8c665154f2dde30e61f0737e098d99cf1e629c05
oshelton <jowenshelton@gmail.com>
2023-11-14 21:19:08 -0500

    Apply Prettier formatting.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,16 +10,15 @@
 <Heading tag="h1" class="mb-8">Welcome!</Heading>
 
 <P class="mb-4">
-	This is the profile/portfolio site of Jack Owen Shelton (there's a little picture of me in the top left).
-	If the name and face doesn't ring a bell you are probably in the wrong place.
+	This is the profile/portfolio site of Jack Owen Shelton (there's a little picture of me in the top
+	left). If the name and face doesn't ring a bell you are probably in the wrong place.
 </P>
 
 <P class="mb-4">
-	This site is dedicated to various parts of my life and projects I have worked on over the years and giving visitors a better picture of who I am.
+	This site is dedicated to various parts of my life and projects I have worked on over the years
+	and giving visitors a better picture of who I am.
 </P>
 
-<P class="mb-8">
-	At some point it may even host a blog.
-</P>
+<P class="mb-8">At some point it may even host a blog.</P>
 
 <Heading tag="h5">Thank you for visiting and enjoy your stay!</Heading>
