6b30aac0afaf8e6ede592f642d37297669966d4d
oshelton <jowenshelton@gmail.com>
2024-03-02 12:45:00 -0500

    #34 -Add date searching support to blog post searching.

--- src/lib/blog/SearchHelpers.js
+++ src/lib/blog/SearchHelpers.js
@@ -1,6 +1,54 @@
 import Enumerable from 'linq';
 
-import { TagsToPostIds } from './SearchData';
+import { PostIdsAndPostedDate, TagsToPostIds } from './SearchData';
+
+/**
+ * Get the ids for posts made after the provided date.
+ * @param {string} date - Date to get posts at or after, must be int he format mm/dd/yyyy.
+ * @returns {string[]}
+ */
+export function GetPostsPostedOnOrAfter(date) {
+	if (!date) {
+		throw new Error('Date must be provided');
+	}
+
+	var splitComponents = date.split('/');
+	var parsedDate = new Date(
+		splitComponents[2],
+		Number.parseInt(splitComponents[0]) - 1,
+		splitComponents[1]
+	);
+	console.log(parsedDate.toDateString());
+
+	return Enumerable.from(PostIdsAndPostedDate)
+		.where((x) => x.posted >= parsedDate)
+		.select((x) => x.id)
+		.toArray();
+}
+
+/**
+ * Get the ids for posts made before the provided date.
+ * @param {string} date - Date to get posts at or after, must be in the format of mm/dd/yyyy.
+ * @returns {string[]}
+ */
+export function GetPostsPostedOnOrBefore(date) {
+	if (!date) {
+		throw new Error('Date must be provided');
+	}
+
+	var splitComponents = date.split('/');
+	var parsedDate = new Date(
+		splitComponents[2],
+		Number.parseInt(splitComponents[0]) - 1,
+		splitComponents[1]
+	);
+	console.log(parsedDate.toDateString());
+
+	return Enumerable.from(PostIdsAndPostedDate)
+		.where((x) => x.posted <= parsedDate)
+		.select((x) => x.id)
+		.toArray();
+}
 
 /**
  * Get all post ids for the posts which have one or more of the provided tags.
--- src/routes/blog/search/+page.svelte
+++ src/routes/blog/search/+page.svelte
@@ -8,13 +8,18 @@
 	import { browser } from '$app/environment';
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
-	import { A, Badge, Button, Checkbox, Modal, Toggle, P } from 'flowbite-svelte';
+	import { A, Badge, Button, Checkbox, Input, Modal, Toggle, P } from 'flowbite-svelte';
+	import SveltyPicker from 'svelty-picker';
 
 	import { GetItemForUrl } from '$lib/navigation';
 	import PageTitleBlock from '$lib/components/PageTitleBlock.svelte';
 	import { CountPageVisit } from '$lib/visitor';
 	import { AllTags } from '$lib/blog/SearchData';
-	import { GetPostIdsForTags } from '$lib/blog/SearchHelpers';
+	import {
+		GetPostsPostedOnOrAfter,
+		GetPostsPostedOnOrBefore,
+		GetPostIdsForTags
+	} from '$lib/blog/SearchHelpers';
 	import { BuildUrlForPost, GetMetadatasForPostIds } from '$lib/blog/PostsHelpers';
 
 	CountPageVisit();
@@ -29,29 +34,27 @@
 	/** @type {import('$lib/types').SearchPostPreferences} */
 	const parsedPreferences = preferences && JSON.parse(preferences);
 
-	/**
-	 * If searching by tags is enabled.
-	 * @type {boolean}
-	 */
+	/** If searching by at least posted by date is enabled. @type {boolean} */
+	let searchAtLeastPostedBy = parsedPreferences?.searchAtLeastPostedBy ?? false;
+	/** The date to at least be posted by when searching. @type {Date} */
+	let searchAtLeastPostedByDate = parsedPreferences?.searchAtLeastPostedByDate;
+
+	/** If searching by no later posted by date is enabled. @type {boolean} */
+	let searchNoLaterPostedBy = parsedPreferences?.searchNoLaterPostedBy ?? false;
+	/** The date to no later be posted by when searching. @type {Date} */
+	let searchNoLaterPostedByDate = parsedPreferences?.searchNoLaterPostedByDate;
+
+	/** If searching by tags is enabled. @type {boolean} */
 	let searchTags = parsedPreferences?.searchTags ?? false;
-
-	/**
-	 * Selected tags for searching.
-	 * @type {string[]}
-	 */
+	/** Selected tags for searching. @type {string[]} */
 	let selectedSearchTags = parsedPreferences?.selectedSearchTags ?? [];
-
-	/**
-	 * If the tag selector dialog is open or not.
-	 * @type {boolean}
-	 */
+	/** If the tag selector dialog is open or not. @type {boolean} */
 	let isTagSelectorOpen = false;
 
-	/**
-	 * Array of matching post metadata.
-	 * @type {import('$lib/types').PostMetadata[]}
-	 */
+	/** Array of matching post metadata. @type {import('$lib/types').PostMetadata[]} */
 	let foundPostMetadata = [];
+
+	//browser && localStorage.setItem('theme', 'dark');
 
 	// Save Preferences.
 	onMount(() => {
@@ -61,6 +64,10 @@
 			localStorage.setItem(
 				'SearchPreferences',
 				JSON.stringify({
+					searchAtLeastPostedBy: searchAtLeastPostedBy,
+					searchAtLeastPostedByDate: searchAtLeastPostedByDate,
+					searchNoLaterPostedBy: searchNoLaterPostedBy,
+					searchNoLaterPostedByDate: searchNoLaterPostedByDate,
 					searchTags: searchTags,
 					selectedSearchTags: selectedSearchTags
 				})
@@ -72,8 +79,39 @@
 	 * Find posts matching selected criteria.
 	 */
 	function searchForPosts() {
-		const postIdsMatchingTags = GetPostIdsForTags(selectedSearchTags);
-		foundPostMetadata = GetMetadatasForPostIds(postIdsMatchingTags)
+		let postIdsToFetch = [];
+		if (searchAtLeastPostedBy && searchAtLeastPostedByDate) {
+			postIdsToFetch.push(GetPostsPostedOnOrAfter(searchAtLeastPostedByDate));
+		}
+
+		if (searchNoLaterPostedBy && searchNoLaterPostedByDate) {
+			postIdsToFetch.push(GetPostsPostedOnOrBefore(searchNoLaterPostedByDate));
+		}
+
+		if (searchTags && selectedSearchTags && selectedSearchTags.length > 0) {
+			postIdsToFetch.push(GetPostIdsForTags(selectedSearchTags));
+		}
+
+		/** @type {Object.<string, number>} */
+		let counts = [];
+		postIdsToFetch.forEach((id) => {
+			if (!counts[id]) {
+				counts[id] = 1;
+			} else {
+				counts[id]++;
+			}
+		});
+
+		const requiredIdCount =
+			(searchAtLeastPostedBy ? 1 : 0) + (searchNoLaterPostedBy ? 1 : 0) + (searchTags ? 1 : 0);
+		postIdsToFetch.length = 0;
+		for (const [key, value] of Object.entries(counts)) {
+			if (value >= requiredIdCount) {
+				postIdsToFetch.push(key);
+			}
+		}
+
+		foundPostMetadata = GetMetadatasForPostIds(postIdsToFetch)
 			.orderByDescending((x) => x.posted)
 			.toArray();
 
@@ -87,7 +125,72 @@
 	markdown={pageDescription}
 />
 
+<!-- At least posted by filter. -->
 <div class="flex gap-2 flex-wrap items-center">
+	<Toggle bind:checked={searchAtLeastPostedBy}>Posted at least by:</Toggle>
+
+	<SveltyPicker
+		bind:value={searchAtLeastPostedByDate}
+		mode="date"
+		displayFormat="mm/dd/yyyy"
+		format="mm/dd/yyyy"
+		disabled={!searchAtLeastPostedBy}
+	>
+		<svelte:fragment
+			slot="inputs"
+			let:disabled
+			let:displayValue
+			let:onInputFocus
+			let:onInputBlur
+			let:onKeyDown
+		>
+			<Input
+				size="sm"
+				value={displayValue}
+				on:keydown={onKeyDown}
+				on:click={onInputFocus}
+				on:blur={onInputBlur}
+				{disabled}
+				readonly
+			/>
+		</svelte:fragment>
+	</SveltyPicker>
+</div>
+
+<!-- Posted no later than by filter. -->
+<div class="flex gap-2 flex-wrap items-center mt-2">
+	<Toggle bind:checked={searchNoLaterPostedBy}>Posted no later than:</Toggle>
+
+	<SveltyPicker
+		bind:value={searchNoLaterPostedByDate}
+		mode="date"
+		displayFormat="mm/dd/yyyy"
+		format="mm/dd/yyyy"
+		disabled={!searchNoLaterPostedBy}
+	>
+		<svelte:fragment
+			slot="inputs"
+			let:disabled
+			let:displayValue
+			let:onInputFocus
+			let:onInputBlur
+			let:onKeyDown
+		>
+			<Input
+				size="sm"
+				value={displayValue}
+				on:keydown={onKeyDown}
+				on:click={onInputFocus}
+				on:blur={onInputBlur}
+				{disabled}
+				readonly
+			/>
+		</svelte:fragment>
+	</SveltyPicker>
+</div>
+
+<!-- Tag bassed searching. -->
+<div class="flex gap-2 flex-wrap items-center mt-2">
 	<Toggle bind:checked={searchTags}>Tags</Toggle>
 
 	<div class="hidden gap-3 flex-wrap sm:flex">
@@ -123,7 +226,9 @@
 
 <Button
 	class="place-self-start mt-3"
-	disabled={!searchTags || selectedSearchTags.length === 0}
+	disabled={(!searchAtLeastPostedBy || !searchAtLeastPostedByDate) &&
+		(!searchNoLaterPostedBy || !searchNoLaterPostedByDate) &&
+		(!searchTags || selectedSearchTags.length === 0)}
 	on:click={searchForPosts}
 >
 	Search Posts
