e40ca60e1109bfafaf330e96412cadd3effd8a85
oshelton <jowenshelton@gmail.com>
2024-03-26 19:42:28 -0400

    Run formatting pass.

--- src/lib/components/HeadingLink.svelte
+++ src/lib/components/HeadingLink.svelte
@@ -6,22 +6,22 @@
 <script>
 	import { A, Heading } from 'flowbite-svelte';
 
-    /** @type {string} */
-    export let id = '';
+	/** @type {string} */
+	export let id = '';
 
-    /** @type {string} Tag to apply to the heading.*/
-    export let tag = "h4";
+	/** @type {string} Tag to apply to the heading.*/
+	export let tag = 'h4';
 
-    /** @type {string} Classess to apply to the link part fo the heading.*/
-    export let aClass = '';
+	/** @type {string} Classess to apply to the link part fo the heading.*/
+	export let aClass = '';
 
 	/** @type {string} Classes to apply to the Heading.*/
 	export let headingClass = '';
 </script>
 
 <A href={`#${id}`} class={`group ${aClass}`}>
-    <Heading {id} {tag} class={`text-primary-600 dark:text-primary-500 ${headingClass}`}>
-        <slot/>
-        <span class="collapse group-hover:visible">#</span>
-    </Heading>
+	<Heading {id} {tag} class={`text-primary-600 dark:text-primary-500 ${headingClass}`}>
+		<slot />
+		<span class="collapse group-hover:visible">#</span>
+	</Heading>
 </A>
